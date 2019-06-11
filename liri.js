623f9e355c4ec501041f3206a8a50aea60ff9cf0
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-06-10 20:39:34 -0700

    artist name, spotify search

--- liri.js
+++ liri.js
@@ -7,3 +7,40 @@ var moment = require("moment");
 var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 
+//all of the functions
+
+//this gets the artist name
+var getArtistNames = function(artist) {
+    return artist.name;
+  };
+
+
+  var getMeSpotify = function(songName) {
+    if (songName === undefined) {
+      songName = "What's my age again";
+    }
+  
+    spotify.search(
+      {
+        type: "track",
+        query: songName
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+  
+        var songs = data.tracks.items;
+  
+        for (var i = 0; i < songs.length; i++) {
+          console.log(i);
+          console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+          console.log("song name: " + songs[i].name);
+          console.log("preview song: " + songs[i].preview_url);
+          console.log("album: " + songs[i].album.name);
+          console.log("-----------------------------------");
+        }
+      }
+    );
+  };
